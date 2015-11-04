052d61702dc0bbbc2e495423d21ba23c45bdacca
Patrick Robertson <Patrick@Patricks-MacBook-Pro.local>
2015-11-04 21:56:08 +1100

    Parsed csv file

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Patrick Robertson on 4/11/2015.
+//  Copyright © 2015 Patrick Robertson. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalizedString
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Patrick Robertson on 4/11/2015.
+//  Copyright © 2015 Patrick Robertson. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+}
